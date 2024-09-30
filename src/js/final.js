cd373c01f7bf2937e54e37d461855b551602df94
jina-ghimire <jinaghimire19@gmail.com>
2024-09-30 12:16:24 +0900

    Add new images, update dist files, and modify configuration

--- src/js/final.js
+++ src/js/final.js
@@ -6,13 +6,13 @@ new Swiper(".swiper-container", {
       clickable: true,
     },
   });
-  
+
   function clicking(){
     let div=document.querySelector(".brands");
     let button=document.querySelector("#myid");
-    
+
     if( button.innerHTML==="Show All"){
-        
+
         button.innerHTML="Hide";
         div.style.height="450px";
         div.style.overflow ="visible";
@@ -21,26 +21,32 @@ new Swiper(".swiper-container", {
      else{
         button.innerHTML="Show All";
         div.style.height="210px";
-        div.style.overflow ="hidden";        
+        div.style.overflow ="hidden";
         }
 
     }
+    window.clicking = clicking;
+
     function hit() {
       let y=document.querySelector(".brands-2");
       let z =document.querySelector("#brands-2__id");
       if( z.innerHTML==="Show All"){
-        
+
         z.innerHTML="Hide";
         y.style.height="500px";
         y.style.overflow ="visible";
     }
 
+
+
      else{
         z.innerHTML="Show All";
         y.style.height="180px";
-        y.style.overflow ="hidden";        
+        y.style.overflow ="hidden";
         }
     }
+    window.hit = hit;
+
 
     function clicked(){
       let x=document.querySelector(".container");
@@ -53,32 +59,45 @@ new Swiper(".swiper-container", {
           x.style.display='none';
         }
     })
+    window.clicked = clicked;
+
 
     function clack(){
       let a=document.querySelector(".feedback");
         a.style.display = (a.style.display === "none" || a.style.display === "") ? "block" : "none";
     }
+    window.clack = clack;
+
 
     function clock(){
       let b=document.querySelector(".modal-call");
       b.style.display = (b.style.display === "none" || b.style.display === "") ? "block" : "none";
     }
-    
+    window.clock = clock;
+
+
     function closethis(){
       let a=document.querySelector(".feedback");
       if(a.style.display==="block"){
         a.style.display="none";
       }
     }
+    window.closethis = closethis;
+
+
     function closeit() {
       let x=document.querySelector(".container");
       if(x.style.display==="block"){
         x.style.display="none";
     }
   }
+  window.closeit = closeit;
+
+
   function closing() {
     let b=document.querySelector(".modal-call");
     if(b.style.display==="block"){
       b.style.display="none";
   }
 }
+window.closing = closing;
--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
       inject: true,
       minify: {
         removeComments: true,
-        collapseWhitespace: false,
+        collapseWhitespace: true,
       }
     }),
 
@@ -93,7 +93,7 @@ module.exports = {
   devServer: {
     static: path.join(__dirname, 'dist'), // Updated from contentBase
     compress: true,
-    port: 9000,
+    port: 9005,
     hot: true, // Enable Hot Module Replacement
     watchFiles: {
       paths: ['src/**/*'], // Watch for changes in source files
